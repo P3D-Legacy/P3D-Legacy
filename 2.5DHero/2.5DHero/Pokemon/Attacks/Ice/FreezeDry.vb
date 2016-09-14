dc63d26858265610e93e15a9e388db16195e063c
Capt_Segis <F20121237@outlook.com>
2016-09-13 22:45:58 -0500

    Create FreezeDry.vb
    using IceBeam as template

---
+++ 2.5DHero/2.5DHero/Pokemon/Attacks/Ice/FreezeDry.vb
@@ -0,0 +1,76 @@
+Namespace BattleSystem.Moves.Ice
+
+    Public Class FreezeDry
+
+        Inherits Attack
+
+        Public Sub New()
+            '#Definitions
+            Me.Type = New Element(Element.Types.Ice)
+            Me.ID = 58
+            Me.OriginalPP = 20
+            Me.CurrentPP = 20
+            Me.MaxPP = 20
+            Me.Power = 70
+            Me.Accuracy = 100
+            Me.Category = Categories.Special
+            Me.ContestCategory = ContestCategories.Beauty
+            Me.Name = "Ice Beam"
+            Me.Description = "The user rapidly cools the target. This may also leave the target frozen. This move is super effective on Water types."
+            Me.CriticalChance = 1
+            Me.IsHMMove = False
+            Me.Target = Targets.OneAdjacentTarget
+            Me.Priority = 0
+            Me.TimesToAttack = 1
+            '#End
+
+            '#SpecialDefinitions
+            Me.MakesContact = False
+            Me.ProtectAffected = True
+            Me.MagicCoatAffected = False
+            Me.SnatchAffected = False
+            Me.MirrorMoveAffected = True
+            Me.KingsrockAffected = False
+            Me.CounterAffected = False
+
+            Me.DisabledWhileGravity = False
+            Me.UseEffectiveness = True
+            Me.ImmunityAffected = True
+            Me.RemovesFrozen = False
+            Me.HasSecondaryEffect = True
+
+            Me.IsHealingMove = False
+            Me.IsRecoilMove = False
+            Me.IsPunchingMove = False
+            Me.IsDamagingMove = True
+            Me.IsProtectMove = False
+            Me.IsSoundMove = False
+
+            Me.IsAffectedBySubstitute = True
+            Me.IsOneHitKOMove = False
+            Me.IsWonderGuardAffected = True
+            '#End
+
+            Me.AIField1 = AIField.Damage
+            Me.AIField2 = AIField.CanFreeze
+
+            EffectChances.Add(10)
+        End Sub
+
+        Public Overrides Sub MoveHits(own As Boolean, BattleScreen As BattleScreen)
+            Dim p As Pokemon = BattleScreen.OwnPokemon
+            Dim op As Pokemon = BattleScreen.OppPokemon
+            If own = False Then
+                p = BattleScreen.OppPokemon
+                op = BattleScreen.OwnPokemon
+            End If
+
+            Dim chance As Integer = GetEffectChance(0, own, BattleScreen)
+            If Core.Random.Next(0, 100) < chance Then
+                BattleScreen.Battle.InflictFreeze(Not own, own, BattleScreen, "", "move:icebeam")
+            End If
+        End Sub
+
+    End Class
+
+End Namespace
