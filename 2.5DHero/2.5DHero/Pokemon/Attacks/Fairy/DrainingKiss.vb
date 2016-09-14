ba46577904bf91317e624fbf97dbafc0e05f75bf
Capt_Segis <F20121237@outlook.com>
2016-09-14 17:00:19 -0500

    Update DrainingKiss.vb

--- 2.5DHero/2.5DHero/Pokemon/Attacks/Fairy/DrainingKiss.vb
+++ 2.5DHero/2.5DHero/Pokemon/Attacks/Fairy/DrainingKiss.vb
@@ -36,7 +36,7 @@ Namespace BattleSystem.Moves.Fairy
             Me.DisabledWhileGravity = False
             Me.UseEffectiveness = True
             Me.ImmunityAffected = True
-            Me.HasSecondaryEffect = True
+            Me.HasSecondaryEffect = False
             Me.RemovesFrozen = False
 
             Me.IsHealingMove = True
