4137815c0d00a717b19bca7a0957192871d2fa65
Capt_Segis <F20121237@outlook.com>
2016-09-17 14:56:06 -0500

    Create FrostBreath.vb

---
+++ 2.5DHero/2.5DHero/Pokemon/Attacks/Ice/FrostBreath.vb
@@ -0,0 +1,56 @@
+Namespace BattleSystem.Moves.Ice
+
+    Public Class FrostBreath
+
+        Inherits Attack
+
+        Public Sub New()
+            '#Definitions
+            Me.Type = New Element(Element.Types.Ice)
+            Me.ID = 524
+            Me.OriginalPP = 10
+            Me.CurrentPP = 10
+            Me.MaxPP = 10
+            Me.Power = 60
+            Me.Accuracy = 90
+            Me.Category = Categories.Special
+            Me.ContestCategory = ContestCategories.Beauty
+            Me.Name = "Frost Breath"
+            Me.Description = "The user blows its cold breath on the target. This attack always results in a critical hit."
+            Me.CriticalChance = 1
+            Me.IsHMMove = False
+            Me.Target = Targets.OneAdjacentTarget
+            Me.Priority = 0
+            Me.TimesToAttack = 1
+            '#End
+
+            '#SpecialDefinitions
+            Me.MakesContact = False
+            Me.ProtectAffected = True
+            Me.MagicCoatAffected = False
+            Me.SnatchAffected = False
+            Me.MirrorMoveAffected = True
+            Me.KingsrockAffected = False
+            Me.CounterAffected = False
+
+            Me.DisabledWhileGravity = False
+            Me.UseEffectiveness = True
+            Me.ImmunityAffected = True
+            Me.RemovesFrozen = False
+            Me.HasSecondaryEffect = False
+
+            Me.IsHealingMove = False
+            Me.IsRecoilMove = False
+            Me.IsPunchingMove = False
+            Me.IsDamagingMove = True
+            Me.IsProtectMove = False
+            Me.IsSoundMove = False
+
+            Me.IsAffectedBySubstitute = True
+            Me.IsOneHitKOMove = False
+            Me.IsWonderGuardAffected = True
+            '#End
+
+    End Class
+
+End Namespace
