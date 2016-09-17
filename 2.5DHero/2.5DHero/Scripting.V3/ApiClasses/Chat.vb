3fadf87fde2a5e9442b74859a048df20e599285f
nilllzz <nidresch@googlemail.com>
2016-09-17 15:42:50 +0200

    Added chat api class

---
+++ 2.5DHero/2.5DHero/Scripting.V3/ApiClasses/Chat.vb
@@ -0,0 +1,17 @@
+﻿Imports Pokemon3D.Scripting
+Imports Pokemon3D.Scripting.Adapters
+Imports Pokemon3D.Scripting.Types
+
+Namespace Scripting.V3.ApiClasses
+
+    <ApiClass("Chat")>
+    Friend NotInheritable Class ChatWrapper
+
+        Public Shared Function clear(processor As ScriptProcessor, parameters As SObject()) As SObject
+            Chat.ClearChat()
+            Return ScriptInAdapter.GetUndefined(processor)
+        End Function
+
+    End Class
+
+End Namespace
