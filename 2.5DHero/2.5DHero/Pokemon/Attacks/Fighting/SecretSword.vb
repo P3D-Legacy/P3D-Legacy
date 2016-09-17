75d49865dd07e372640382acf048d1a6665e4c0f
Capt_Segis <F20121237@outlook.com>
2016-09-17 17:44:47 -0500

    Create SecretSword.vb

---
+++ 2.5DHero/2.5DHero/Pokemon/Attacks/Fighting/SecretSword.vb
@@ -0,0 +1,61 @@
+﻿Namespace BattleSystem.Moves.Fighting
+
+    Public Class SecretSword
+
+        Inherits Attack
+
+        Public Sub New()
+            '#Definitions
+            Me.Type = New Element(Element.Types.Fighting)
+            Me.ID = 548
+            Me.OriginalPP = 10
+            Me.CurrentPP = 10
+            Me.MaxPP = 10
+            Me.Power = 85
+            Me.Accuracy = 100
+            Me.Category = Categories.Special
+            Me.ContestCategory = ContestCategories.Beauty
+            Me.Name = "Secret Sword"
+            Me.Description = "The user cuts with its long horn. The odd power contained in the horn does physical damage to the target."
+            Me.CriticalChance = 1
+            Me.IsHMMove = False
+            Me.Target = Targets.OneAdjacentTarget
+            Me.Priority = 0
+            Me.TimesToAttack = 1
+            '#End
+
+            '#SpecialDefinitions
+            Me.MakesContact = False
+            Me.ProtectAffected = True
+            Me.MagicCoatAffected = False
+            Me.SnatchAffected = False
+            Me.MirrorMoveAffected = True
+            Me.KingsrockAffected = True
+            Me.CounterAffected = False
+
+            Me.DisabledWhileGravity = False
+            Me.UseEffectiveness = True
+            Me.ImmunityAffected = True
+            Me.RemovesFrozen = False
+            Me.HasSecondaryEffect = False
+
+            Me.IsHealingMove = False
+            Me.IsRecoilMove = False
+            Me.IsPunchingMove = False
+            Me.IsDamagingMove = True
+            Me.IsProtectMove = False
+            Me.IsSoundMove = False
+
+            Me.IsAffectedBySubstitute = True
+            Me.IsOneHitKOMove = False
+            Me.IsWonderGuardAffected = True
+            '#End
+        End Sub
+
+        Public Overrides Function GetUseDefenseStat(p As Pokemon) As Integer
+            Return p.Defense
+        End Function
+
+    End Class
+
+End Namespace
