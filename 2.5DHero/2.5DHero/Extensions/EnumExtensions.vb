6595707e06404c94aaf095829d9b5a73e13c3d88
Aragas <micvitalij@yandex.ru>
2016-09-19 17:25:43 +0300

    Added reflection for Attack classes

---
+++ 2.5DHero/2.5DHero/Extensions/EnumExtensions.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Reflection
+Imports System.Runtime.CompilerServices
+
+Public Module EnumExtensions
+    <Extension> _
+    Public Function SomethingConstructorArray(Of TSomething)(somethingTypeEnum As [Enum], assemblyWhereToSearch As Assembly) As Func(Of TSomething)()
+        Dim somethingTypeNameArray = [Enum].GetValues(somethingTypeEnum.[GetType]())
+        Dim sometingConstructorArray = New Func(Of TSomething)(somethingTypeNameArray.Cast(Of Integer)().Max()) {}
+
+        For Each attackTypeName In somethingTypeNameArray
+            Dim type = GetTypeFromNameAndAbstract(Of TSomething)(attackTypeName.ToString(), assemblyWhereToSearch)
+            sometingConstructorArray(CInt(attackTypeName)) = If(type IsNot Nothing, DirectCast(Function() DirectCast(Activator.CreateInstance(type), TSomething), Func(Of TSomething)), Nothing)
+        Next
+
+        Return sometingConstructorArray
+    End Function
+
+    Private Function GetTypeFromNameAndAbstract(Of T)(className As String, assembly As Assembly) As Type
+        Return assembly.DefinedTypes.Where(Function(typeInfo) typeInfo.Name = className And typeInfo.IsSubclassOf(GetType(T))).Select(Function(typeInfo) typeInfo.AsType()).FirstOrDefault()
+    End Function
+End Module
---
+++ 2.5DHero/2.5DHero/Pokemon/Attacks/AttackList.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Reflection
+
+Namespace BattleSystem
+
+    Public NotInheritable Class AttackList
+        Public Shared ReadOnly Attacks As Func(Of Attack)()
+
+        Shared Sub New()
+            Attacks = New AttackEnum().SomethingConstructorArray(Of Attack)(Assembly.GetAssembly(GetType(AttackList)))
+        End Sub
+    End Class
+End Namespace
