d9035ac1b57c731a1a84dde86b9863ad562cd57f
nilllzz <nidresch@googlemail.com>
2016-09-20 23:50:16 +0200

    Fixed battle points price & ItemTypes access

--- 2.5DHero/2.5DHero/Pokemon/Items/Machines/TM45.vb
+++ 2.5DHero/2.5DHero/Pokemon/Items/Machines/TM45.vb
@@ -5,11 +5,11 @@ Namespace Items.Machines
 
         Inherits TechMachine
 
+        Public Overrides ReadOnly Property BattlePointsPrice As Integer = 32
+
         Public Sub New()
             MyBase.New(True, 1500, 213)
             CanTeachWhenGender = True
-
-            Me._battlePointsPrice = 32
         End Sub
 
     End Class
