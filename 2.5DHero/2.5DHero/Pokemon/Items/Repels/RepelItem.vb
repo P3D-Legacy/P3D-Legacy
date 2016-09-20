00cedf786e6527907ca91e6270795e0fe63bf547
nilllzz <nidresch@googlemail.com>
2016-09-20 06:29:17 +0200

    Changed repel items.

---
+++ 2.5DHero/2.5DHero/Pokemon/Items/Repels/RepelItem.vb
@@ -0,0 +1,27 @@
+﻿Namespace Items.Repels
+
+    Public MustInherit Class RepelItem
+
+        Inherits Item
+
+        Public Overrides ReadOnly Property CanBeUsedInBattle As Boolean = False
+
+        Public MustOverride ReadOnly Property RepelSteps As Integer
+
+        Public Overrides Sub Use()
+            If Core.Player.RepelSteps <= 0 Then
+                Core.Player.Inventory.RemoveItem(ID, 1)
+                Player.Temp.LastUsedRepel = ID
+
+                SoundManager.PlaySound("repel_use", False)
+                Screen.TextBox.Show(Core.Player.Name & " used a~" & Name, {}, True, True)
+                Core.Player.RepelSteps = RepelSteps
+                PlayerStatistics.Track("[42]Repels used", 1)
+            Else
+                Screen.TextBox.Show("The Repel is still~in effect.", {}, True, True)
+            End If
+        End Sub
+
+    End Class
+
+End Namespace
--- 2.5DHero/2.5DHero/Pokemon/Items/Repels/SuperRepel.vb
+++ 2.5DHero/2.5DHero/Pokemon/Items/Repels/SuperRepel.vb
@@ -3,29 +3,14 @@ Namespace Items.Repels
     <Item(42, "Super Repel")>
     Public Class SuperRepel
 
-        Inherits Item
+        Inherits RepelItem
+
+        Public Overrides ReadOnly Property Description As String = "An item that prevents any low-level wild Pokémon from jumping out at you for 200 steps after its use."
+        Public Overrides ReadOnly Property PokeDollarPrice As Integer = 500
+        Public Overrides ReadOnly Property RepelSteps As Integer = 200
 
         Public Sub New()
-            MyBase.New("Super Repel", 500, ItemTypes.Standard, 42, 1, 0, New Rectangle(432, 24, 24, 24), "An item that prevents any low-level wild Pokémon from jumping out at you for 200 steps after its use.")
-
-            Me._canBeHold = True
-            Me._canBeTraded = True
-            Me._canBeUsed = True
-            Me._canBeUsedInBattle = False
-        End Sub
-
-        Public Overrides Sub Use()
-            If Core.Player.RepelSteps <= 0 Then
-                Core.Player.Inventory.RemoveItem(Me.ID, 1)
-                Player.Temp.LastUsedRepel = Me.ID
-
-                SoundManager.PlaySound("repel_use", False)
-                Screen.TextBox.Show(Core.Player.Name & " used a~" & Me.Name, {}, True, True)
-                Core.Player.RepelSteps = 200
-                PlayerStatistics.Track("[42]Repels used", 1)
-            Else
-                Screen.TextBox.Show("The Repel is still~in effect.", {}, True, True)
-            End If
+            _textureRectangle = New Rectangle(432, 24, 24, 24)
         End Sub
 
     End Class
