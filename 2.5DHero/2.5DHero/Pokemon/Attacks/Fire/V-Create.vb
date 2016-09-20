c4236d3fb297f080bb26554fd0693d1d7ec785c6
Capt_Segis <F20121237@outlook.com>
2016-09-20 11:16:35 -0500

    Create V-Create.vb

---
+++ 2.5DHero/2.5DHero/Pokemon/Attacks/Fire/V-Create.vb
@@ -0,0 +1,63 @@
+Namespace BattleSystem.Moves.Fire
+
+    Public Class V-Create
+
+        Inherits Attack
+
+        Public Sub New()
+            '#Definitions
+            Me.Type = New Element(Element.Types.Fire)
+            Me.ID = 557
+            Me.OriginalPP = 5
+            Me.CurrentPP = 5
+            Me.MaxPP = 5
+            Me.Power = 180
+            Me.Accuracy = 95
+            Me.Category = Categories.Physical
+            Me.ContestCategory = ContestCategories.Cool
+            Me.Name = "V-Create"
+            Me.Description = "With a hot flame on its forehead, the user hurls itself at its target. This lowers the user's Defense, Sp. Def, and Speed stats."
+            Me.CriticalChance = 1
+            Me.IsHMMove = False
+            Me.Target = Targets.OneAdjacentTarget
+            Me.Priority = 0
+            Me.TimesToAttack = 1
+            '#End
+
+            '#SpecialDefinitions
+            Me.MakesContact = False
+            Me.ProtectAffected = True
+            Me.MagicCoatAffected = False
+            Me.SnatchAffected = False
+            Me.MirrorMoveAffected = True
+            Me.KingsrockAffected = True
+            Me.CounterAffected = False
+
+            Me.DisabledWhileGravity = False
+            Me.UseEffectiveness = True
+            Me.ImmunityAffected = True
+            Me.HasSecondaryEffect = False
+            Me.RemovesFrozen = False
+
+            Me.IsHealingMove = False
+            Me.IsRecoilMove = False
+            Me.IsPunchingMove = False
+            Me.IsDamagingMove = True
+            Me.IsProtectMove = False
+            Me.IsSoundMove = False
+
+            Me.IsAffectedBySubstitute = True
+            Me.IsOneHitKOMove = False
+            Me.IsWonderGuardAffected = True
+            '#End
+        End Sub
+
+        Public Overrides Sub MoveHits(own As Boolean, BattleScreen As BattleScreen)
+                    BattleScreen.Battle.LowerStat(own, own, BattleScreen, "Defense", 1, "", "move:v-create")        
+                    BattleScreen.Battle.LowerStat(own, own, BattleScreen, "Special Defense", 1, "", "move:v-create")
+                    BattleScreen.Battle.LowerStat(own, own, BattleScreen, "Speed", 1, "", "move:v-create")                    
+        End Sub
+
+    End Class
+
+End Namespace
