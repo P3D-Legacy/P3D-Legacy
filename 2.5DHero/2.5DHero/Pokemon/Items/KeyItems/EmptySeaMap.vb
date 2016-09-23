562c2fb3e067d908a32d9514dc7ec187c71ff2eb
jianmingyong <jianming1993@gmail.com>
2016-09-23 13:52:23 +0800

    Fix typo in EmptySealMap.vb

--- 2.5DHero/2.5DHero/Pokemon/Items/KeyItems/EmptySeaMap.vb
+++ 2.5DHero/2.5DHero/Pokemon/Items/KeyItems/EmptySeaMap.vb
@@ -1,16 +1,16 @@
-+Namespace Items.KeyItems
- +
- +    <Item(292, "Empty Sea Map")>
- +    Public Class EmptySeaMap
- +
- +        Inherits KeyItem
- +
- +        Public Overrides ReadOnly Property Description As String = "A sea chart on an odd blue parchment showing a point in the open ocean."
- +
- +        Public Sub New()
- +            _textureRectangle = New Rectangle(192, 264, 24, 24)
- +        End Sub
- +
- +    End Class
- +
- +End Namespace
+Namespace Items.KeyItems
+
+    <Item(292, "Empty Sea Map")>
+    Public Class EmptySeaMap
+
+        Inherits KeyItem
+
+        Public Overrides ReadOnly Property Description As String = "A sea chart on an odd blue parchment showing a point in the open ocean."
+
+        Public Sub New()
+            _textureRectangle = New Rectangle(192, 264, 24, 24)
+        End Sub
+
+    End Class
+
+End Namespace
