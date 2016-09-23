3a92b89073691440ee33596cef51b87f54921b2d
FantaX1911 <Qusaiab@hotmail.com>
2016-09-23 15:32:59 +0300

    Added the remaining Incenses items.
    Full, Lax, Luck, Pure, Rock and Rose incense added.

---
+++ 2.5DHero/2.5DHero/Pokemon/Items/Standard/LuckIncense.vb
@@ -0,0 +1,20 @@
+Namespace Items.Standard
+
+    <Item(290, "Luck Incense")>
+    Public Class LuckIncense
+
+        Inherits Item
+
+        Public Overrides ReadOnly Property Description As String = "An item to be held by a Pokémon. The beguiling aroma of this incense may cause attacks to miss its holder."
+        Public Overrides ReadOnly Property BattlePointsPrice As Integer = 9600
+        Public Overrides ReadOnly Property FlingDamage As Integer = 10
+        Public Overrides ReadOnly Property CanBeUsedInBattle As Boolean = False
+        Public Overrides ReadOnly Property CanBeUsed As Boolean = False
+
+        Public Sub New()
+            _textureRectangle = New Rectangle(264, 264, 24, 24)
+        End Sub
+
+    End Class
+
+End Namespace
---
+++ 2.5DHero/2.5DHero/Pokemon/Items/Standard/RockIncense.vb
@@ -0,0 +1,20 @@
+Namespace Items.Standard
+
+    <Item(286, "Rock Incense")>
+    Public Class RockIncense
+
+        Inherits Item
+
+        Public Overrides ReadOnly Property Description As String = "An item to be held by a Pokémon. This exotic-smelling incense boosts the power of Rock-type moves."
+        Public Overrides ReadOnly Property BattlePointsPrice As Integer = 9600
+        Public Overrides ReadOnly Property FlingDamage As Integer = 10
+        Public Overrides ReadOnly Property CanBeUsedInBattle As Boolean = False
+        Public Overrides ReadOnly Property CanBeUsed As Boolean = False
+
+        Public Sub New()
+            _textureRectangle = New Rectangle(312, 264, 24, 24)
+        End Sub
+
+    End Class
+
+End Namespace
