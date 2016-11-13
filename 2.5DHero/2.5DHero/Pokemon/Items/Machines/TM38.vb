c8154a3f1c241ea7631d914c9f5307bc7cde38bc
CaptainSegis <F20121237@outlook.com>
2016-11-13 18:36:08 -0500

    Rearranged dept stores TMs, as per Darkfire's specs. Fixed TM38 price.

--- 2.5DHero/2.5DHero/Pokemon/Items/Machines/TM38.vb
+++ 2.5DHero/2.5DHero/Pokemon/Items/Machines/TM38.vb
@@ -6,7 +6,7 @@ Namespace Items.Machines
         Inherits TechMachine
 
         Public Sub New()
-            MyBase.New(True, 2000, 126)
+            MyBase.New(True, 5500, 126)
         End Sub
 
     End Class
