5e6275fb10a0eb19a7064226fcc324ddf7cade1c
SViper <venuzauras@gmail.com>
2016-11-19 16:28:01 +0200

    Create PrimordialSea.vb

---
+++ 2.5DHero/2.5DHero/Pokemon/Abilities/PrimordialSea.vb
@@ -0,0 +1,13 @@
+Namespace Abilities
+
+    Public Class PrimordialSea
+
+        Inherits Ability
+
+        Public Sub New()
+            MyBase.New(189, "Primordial Sea", "The Pokémon changes the weather to nullify Fire-type attacks.")
+        End Sub
+
+    End Class
+
+End Namespace
