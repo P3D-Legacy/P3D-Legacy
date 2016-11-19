18c0d02c0679746ed1a536b6ed9e84890922c036
SViper <venuzauras@gmail.com>
2016-11-19 15:50:40 +0200

    Description.

--- 2.5DHero/2.5DHero/Pokemon/Abilities/ArenaTrap.vb
+++ 2.5DHero/2.5DHero/Pokemon/Abilities/ArenaTrap.vb
@@ -5,7 +5,7 @@ Namespace Abilities
         Inherits Ability
 
         Public Sub New()
-            MyBase.New(71, "Arena Trap", "Prevents the foe from fleeing.")
+            MyBase.New(71, "Arena Trap", "Prevents opposing Pokémon from fleeing.")
         End Sub
 
     End Class
