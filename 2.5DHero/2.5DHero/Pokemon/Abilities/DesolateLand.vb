8c9f80ed7532262a21caadef9e5321987f4a0fd2
SViper <venuzauras@gmail.com>
2016-11-19 16:32:47 +0200

    Create DesolateLand.vb

---
+++ 2.5DHero/2.5DHero/Pokemon/Abilities/DesolateLand.vb
@@ -0,0 +1,13 @@
+Namespace Abilities
+
+    Public Class DesolateLand
+
+        Inherits Ability
+
+        Public Sub New()
+            MyBase.New(190, "Desolate Land", "The Pokémon changes the weather to nullify Water-type attacks.")
+        End Sub
+
+    End Class
+
+End Namespace
