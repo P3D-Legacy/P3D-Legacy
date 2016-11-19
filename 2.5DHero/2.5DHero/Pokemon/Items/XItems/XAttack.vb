f03789e34f56f6999a97ea8b26f7038bfbbd5c05
SViper <venuzauras@gmail.com>
2016-11-19 15:34:26 +0200

    Gen 7 Description.

--- 2.5DHero/2.5DHero/Pokemon/Items/XItems/XAttack.vb
+++ 2.5DHero/2.5DHero/Pokemon/Items/XItems/XAttack.vb
@@ -6,7 +6,7 @@ Namespace Items.XItems
         Inherits XItem
 
         Public Overrides ReadOnly Property PokeDollarPrice As Integer = 500
-        Public Overrides ReadOnly Property Description As String = "An item that boosts the Attack stat of a Pokémon during a battle. It wears off once the Pokémon is withdrawn."
+        Public Overrides ReadOnly Property Description As String = "An item that sharply boosts the Attack stat of a Pokémon during a battle. It wears off once the Pokémon is withdrawn."
 
         Public Sub New()
             _textureRectangle = New Rectangle(72, 48, 24, 24)
