7fb1fdfc2afda5bccdc35346bbda3171e5b8451e
SViper <venuzauras@gmail.com>
2016-11-19 15:22:32 +0200

    Gen 7 Description.

--- 2.5DHero/2.5DHero/Pokemon/Items/Medicine/MooMooMilk.vb
+++ 2.5DHero/2.5DHero/Pokemon/Items/Medicine/MooMooMilk.vb
@@ -6,7 +6,7 @@ Namespace Items.Medicine
         Inherits MedicineItem
 
         Public Overrides ReadOnly Property IsHealingItem As Boolean = True
-        Public Overrides ReadOnly Property Description As String = "A bottle of highly nutritious milk. When consumed, it restores 100 HP to an injured Pokémon."
+        Public Overrides ReadOnly Property Description As String = "A bottle of highly nutritious milk. When consumed, it restores up to 100 HP to an injured Pokémon."
         Public Overrides ReadOnly Property PokeDollarPrice As Integer = 500
 
         Public Sub New()
