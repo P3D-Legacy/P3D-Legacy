eb095f0531c151fa4e2768bc361370b8a9b51258
SViper <venuzauras@gmail.com>
2016-11-19 16:19:51 +0200

    Aerilate.

---
+++ 2.5DHero/2.5DHero/Pokemon/Abilities/Aerilate.vb
@@ -0,0 +1,13 @@
+Namespace Abilities
+
+Public Class Aerilate
+
+        Inherits Ability
+
+        Public Sub New()
+            MyBase.New(184, "Aerilate", "Normal-type moves become Flying-type moves. The power of those moves is boosted a little.")
+        End Sub
+
+    End Class
+
+End Namespace
