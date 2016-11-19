31bdf2cc3835b96cedcfe7df8fb36a98da31489f
SViper <venuzauras@gmail.com>
2016-11-19 15:45:13 +0200

    Description Gen 7.

--- 2.5DHero/2.5DHero/Pokemon/Abilities/Aftermath.vb
+++ 2.5DHero/2.5DHero/Pokemon/Abilities/Aftermath.vb
@@ -5,7 +5,7 @@ Namespace Abilities
         Inherits Ability
 
         Public Sub New()
-            MyBase.New(106, "Aftermath", "Damages the attacker landing the finishing hit.")
+            MyBase.New(106, "Aftermath", "Damages the attacker if it contacts the Pokémon with a finishing hit.")
         End Sub
 
     End Class
