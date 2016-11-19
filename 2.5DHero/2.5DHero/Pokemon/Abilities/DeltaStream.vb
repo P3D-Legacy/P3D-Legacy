95c75e7541d29d74eb4cabb14238210c8fb05400
SViper <venuzauras@gmail.com>
2016-11-19 16:34:42 +0200

    Create DeltaStream.vb

---
+++ 2.5DHero/2.5DHero/Pokemon/Abilities/DeltaStream.vb
@@ -0,0 +1,13 @@
+Namespace Abilities
+
+    Public Class DeltaStream
+
+        Inherits Ability
+
+        Public Sub New()
+            MyBase.New(191, "Delta Stream", "The Pokémon changes the weather to eliminate all of the Flying type's weaknesses.")
+        End Sub
+
+    End Class
+
+End Namespace
