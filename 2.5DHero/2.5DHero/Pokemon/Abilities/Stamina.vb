df23ea212a2f69f6523eac7f38280bb201696105
SViper <venuzauras@gmail.com>
2016-11-19 16:36:45 +0200

    Create Stamina.vb

---
+++ 2.5DHero/2.5DHero/Pokemon/Abilities/Stamina.vb
@@ -0,0 +1,13 @@
+Namespace Abilities
+
+    Public Class Stamina
+
+        Inherits Ability
+
+        Public Sub New()
+            MyBase.New(192, "Stamina", "Boosts the Defense stat when hit by an attack.")
+        End Sub
+
+    End Class
+
+End Namespace
