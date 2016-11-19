3a48971e8047290e7a9ed5543e89adcd24b37f3c
SViper <venuzauras@gmail.com>
2016-11-19 15:33:39 +0200

    Description Gen 7.

--- 2.5DHero/2.5DHero/Pokemon/Items/XItems/XAccuracy.vb
+++ 2.5DHero/2.5DHero/Pokemon/Items/XItems/XAccuracy.vb
@@ -6,7 +6,7 @@ Namespace Items.XItems
         Inherits XItem
 
         Public Overrides ReadOnly Property PokeDollarPrice As Integer = 950
-        Public Overrides ReadOnly Property Description As String = "An item that boosts the accuracy of a Pokémon during a battle. It wears off once the Pokémon is withdrawn."
+        Public Overrides ReadOnly Property Description As String = "An item that sharply boosts the accuracy of a Pokémon during a battle. It wears off once the Pokémon is withdrawn."
 
         Public Sub New()
             _textureRectangle = New Rectangle(216, 24, 24, 24)
