66fca812b8a55e23d2de00d8239982e9cb8d04ea
SViper <venuzauras@gmail.com>
2016-11-19 15:20:29 +0200

    Gen 7 Description.

--- 2.5DHero/2.5DHero/Pokemon/Items/Medicine/Lemonade.vb
+++ 2.5DHero/2.5DHero/Pokemon/Items/Medicine/Lemonade.vb
@@ -6,7 +6,7 @@ Namespace Items.Medicine
         Inherits MedicineItem
 
         Public Overrides ReadOnly Property IsHealingItem As Boolean = True
-        Public Overrides ReadOnly Property Description As String = "A very sweet and refreshing drink. When consumed, it restores 70 HP to an injured Pokémon."
+        Public Overrides ReadOnly Property Description As String = "A very sweet and refreshing drink. When consumed, it restores up to 70 HP to an injured Pokémon."
         Public Overrides ReadOnly Property PokeDollarPrice As Integer = 400
 
         Public Sub New()
