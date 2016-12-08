16ce550a83257f9af92a140b7af530bf7909458d
Aragas <micvitalij@yandex.ru>
2016-12-08 17:16:51 +0300

    Graphic fix

--- 2.5DHero/2.5DHero/Entites/Enviroment/WallBlock.vb
+++ 2.5DHero/2.5DHero/Entites/Enviroment/WallBlock.vb
@@ -2,6 +2,10 @@
 
     Inherits Entity
 
+        Protected Overrides Function CalculateCameraDistance(CPosition As Vector3) as Single
+        Return MyBase.CalculateCameraDistance(CPosition) - 0.2F
+    End Function
+
     Public Overrides Sub Render()
         Me.Draw(Me.Model, Textures, False)
     End Sub
