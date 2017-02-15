5a09a359b6e24e6ebf8daa4b86358678c380f1f3
CaptainSegis <F20121237@outlook.com>
2017-02-14 19:03:12 -0500

    Fixed testlevel.dat small freeze bug by adding a cool background music to it.

--- 2.5DHero/2.5DHero/Security/FileValidation.vb
+++ 2.5DHero/2.5DHero/Security/FileValidation.vb
@@ -6,8 +6,8 @@
         Shared _valid As Boolean = False
 
         Const RUNVALIDATION As Boolean = False
-        Const EXPECTEDSIZE As Integer = 42417206
-        Const METAHASH As String = "MUVCMDNFOEZDMTQ2ODEyRjFDNzg5RDI4NDgxMTE0Mjg="
+        Const EXPECTEDSIZE As Integer = 42417354
+        Const METAHASH As String = "RUM3N0U1REM0NzREQjkzRUU2MEE1NDk2MUQxNDU5NDU="
 
         Public Shared ReadOnly Property IsValid(ByVal ForceResult As Boolean) As Boolean
             Get
