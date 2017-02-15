e6d2869cfa21fab2507e82f31c7fd3b416cc8cfb
FantaX1911 <Qusaiab@hotmail.com>
2017-02-15 20:50:47 +0200

    Updated release version to 92 after forgeting it for 2 entire versions!

--- 2.5DHero/2.5DHero/Core/GameController.vb
+++ 2.5DHero/2.5DHero/Core/GameController.vb
@@ -15,7 +15,7 @@ Public Class GameController
     ''' <summary>
     ''' The number of released iterations of the game.
     ''' </summary>
-    Public Const RELEASEVERSION As String = "90"
+    Public Const RELEASEVERSION As String = "92"
 
     ''' <summary>
     ''' The development stage the game is in.
