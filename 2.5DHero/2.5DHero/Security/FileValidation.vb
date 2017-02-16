81c879cd3df0469b56b13651f53e9e3a76cf1560
jianmingyong <jianming1993@gmail.com>
2017-02-16 23:47:30 +0800

    Update Updater.exe, remove CurrentVersion.dat and add missing files into the Content.mgcb

--- 2.5DHero/2.5DHero/Security/FileValidation.vb
+++ 2.5DHero/2.5DHero/Security/FileValidation.vb
@@ -6,8 +6,8 @@
         Shared _valid As Boolean = False
 
         Const RUNVALIDATION As Boolean = False
-        Const EXPECTEDSIZE As Integer = 42417354
-        Const METAHASH As String = "RUM3N0U1REM0NzREQjkzRUU2MEE1NDk2MUQxNDU5NDU="
+        Const EXPECTEDSIZE As Integer = 42417361
+        Const METAHASH As String = "ODNEM0E3NkZFMTNBODk2ODAyODAxRkYzN0U3RTM0Mzk="
 
         Public Shared ReadOnly Property IsValid(ByVal ForceResult As Boolean) As Boolean
             Get
