93e6a97d5316c6b6dcd2147a71ed5320b83c7d36
nilllzz <nidresch@googlemail.com>
2018-02-21 07:46:05 +0100

    update player prototype & upgrade kolben

--- 2.5DHero/2.5DHero/World/ActionScript/V3/Prototypes/PlayerPrototype.vb
+++ 2.5DHero/2.5DHero/World/ActionScript/V3/Prototypes/PlayerPrototype.vb
@@ -36,6 +36,35 @@ Namespace Scripting.V3.Prototypes
 
         End Function
 
+        <ScriptFunction(ScriptFunctionType.Getter, VariableName:="hasPokegear", IsStatic:=True)>
+        <ApiMethodSignature(GetType(Boolean))>
+        Public Shared Function GetHasPokegear(This As Object, objLink As ScriptObjectLink, parameters As Object()) As Object
+
+            Return Core.Player.HasPokegear
+
+        End Function
+
+        <ScriptFunction(ScriptFunctionType.Setter, VariableName:="hasPokegear", IsStatic:=True)>
+        <ApiMethodSignature(GetType(Boolean))>
+        Public Shared Function SetHasPokegear(This As Object, objLink As ScriptObjectLink, parameters As Object()) As Object
+
+            If TypeContract.Ensure(parameters, GetType(Boolean)) Then
+
+                Core.Player.HasPokegear = CType(parameters(0), Boolean)
+
+            End If
+
+            Return NetUndefined.Instance
+
+        End Function
+
+        <ScriptFunction(ScriptFunctionType.Getter, VariableName:="inventory", IsStatic:=True)>
+        <ApiMethodSignature(GetType(InventoryPrototype))>
+        Public Shared Function GetInventory(This As Object, objLink As ScriptObjectLink, parameters As Object()) As Object
+
+            Return New InventoryPrototype()
+
+        End Function
 
     End Class
 
