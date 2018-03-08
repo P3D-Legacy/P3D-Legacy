0e5d75835c1063b90e0694e1a177178daa68813b
CaptainSegis <F20121237@outlook.com>
2018-03-07 21:06:05 -0500

    Included Aerilate, Parental Bond

---
+++ P3D/Pokemon/Abilities/ParentalBond.vb
@@ -0,0 +1,13 @@
+Namespace Abilities
+
+    Public Class ParentalBond
+
+        Inherits Ability
+
+        Public Sub New()
+            MyBase.New(185, "Parental Bond", "Parent and child attack together.")
+        End Sub
+
+    End Class
+
+End Namespace
