29225159251815c3c6ffb6d8633b3eda03d788cb
FantaX1911 <FantaX1911@users.noreply.github.com>
2018-09-14 19:23:41 +0300

    Added Grass knot & Flame charge TMs, and fixed TM147 giving "Chatter" instead of "Flame charge".

--- P3D/Pokemon/Items/Machines/TM147.vb
+++ P3D/Pokemon/Items/Machines/TM147.vb
@@ -6,7 +6,7 @@ Namespace Items.Machines
         Inherits TechMachine
 
         Public Sub New()
-            MyBase.New(True, 3000, 448)
+            MyBase.New(True, 3000, 488)
         End Sub
 
     End Class
