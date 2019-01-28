5e954cb39bfeb450f7982a4c5697a16d0e283f95
darkfire006 <blazer257@live.com>
2019-01-27 21:52:24 -0600

    Version update minutiea

--- P3D/Core/GameController.vb
+++ P3D/Core/GameController.vb
@@ -12,12 +12,12 @@ Public Class GameController
     ''' <summary>
     ''' The current version of the game.
     ''' </summary>
-    Public Const GAMEVERSION As String = "0.55"
+    Public Const GAMEVERSION As String = "0.55.1"
 
     ''' <summary>
     ''' The number of released iterations of the game.
     ''' </summary>
-    Public Const RELEASEVERSION As String = "94"
+    Public Const RELEASEVERSION As String = "95"
 
     ''' <summary>
     ''' The development stage the game is in.
--- P3D/My Project/AssemblyInfo.vb
+++ P3D/My Project/AssemblyInfo.vb
@@ -9,7 +9,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyProduct("Pokémon3D")> 
 <Assembly: AssemblyDescription("")>
 <Assembly: AssemblyCompany("Kolben Games")>
-<Assembly: AssemblyCopyright("Copyright © Kolben 2012-2018")>
+<Assembly: AssemblyCopyright("Copyright © Kolben 2012-2019")>
 <Assembly: AssemblyTrademark("")>
 <Assembly: AssemblyCulture("")>
 
@@ -31,4 +31,4 @@ Imports System.Runtime.InteropServices
 '      Build Number
 '      Revision
 '
-<Assembly: AssemblyVersion("0.55")>
+<Assembly: AssemblyVersion("0.55.1")>
