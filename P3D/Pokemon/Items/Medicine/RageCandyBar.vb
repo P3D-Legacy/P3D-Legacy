74ef66cc28825138ecaac902309680caf6ce59cb
FantaX1911 <FantaX1911@users.noreply.github.com>
2019-01-30 16:57:18 +0200

    Updated all medicine to GenVII

--- P3D/Pokemon/Items/Medicine/RageCandyBar.vb
+++ P3D/Pokemon/Items/Medicine/RageCandyBar.vb
@@ -6,7 +6,7 @@ Namespace Items.Medicine
         Inherits MedicineItem
 
         Public Overrides ReadOnly Property IsHealingItem As Boolean = True
-        Public Overrides ReadOnly Property Description As String = "A famous Mahogany Town candy tourists like to buy and take home. It restores the HP of one Pokémon by 20 points."
+        Public Overrides ReadOnly Property Description As String = "A famous Mahogany Town candy tourists like to buy and take home. It can be used once to heal all the status conditions of a Pokémon."
         Public Overrides ReadOnly Property PokeDollarPrice As Integer = 300
 
         Public Sub New()
@@ -25,7 +25,38 @@ Namespace Items.Medicine
         End Sub
 
         Public Overrides Function UseOnPokemon(ByVal PokeIndex As Integer) As Boolean
-            Return HealPokemon(PokeIndex, 20)
+            Dim Pokemon As Pokemon = Core.Player.Pokemons(PokeIndex)
+
+            If Pokemon.Status = P3D.Pokemon.StatusProblems.Fainted Then
+                Screen.TextBox.reDelay = 0.0F
+                Screen.TextBox.Show(Pokemon.GetDisplayName() & "~is fainted!", {})
+
+                Return False
+            Else
+                If Pokemon.Status <> P3D.Pokemon.StatusProblems.None Or Pokemon.HasVolatileStatus(P3D.Pokemon.VolatileStatus.Confusion) = True Then
+                    Pokemon.Status = P3D.Pokemon.StatusProblems.None
+
+                    If Pokemon.HasVolatileStatus(P3D.Pokemon.VolatileStatus.Confusion) = True Then
+                        Pokemon.RemoveVolatileStatus(P3D.Pokemon.VolatileStatus.Confusion)
+                    End If
+
+                    Screen.TextBox.reDelay = 0.0F
+
+                    Dim t As String = Pokemon.GetDisplayName() & "~gets healed up!"
+                    t &= RemoveItem()
+
+                    SoundManager.PlaySound("single_heal", False)
+                    Screen.TextBox.Show(t, {})
+                    PlayerStatistics.Track("[17]Medicine Items used", 1)
+
+                    Return True
+                Else
+                    Screen.TextBox.reDelay = 0.0F
+                    Screen.TextBox.Show(Pokemon.GetDisplayName() & "~is fully healed!", {}, True, True)
+
+                    Return False
+                End If
+            End If
         End Function
 
     End Class
