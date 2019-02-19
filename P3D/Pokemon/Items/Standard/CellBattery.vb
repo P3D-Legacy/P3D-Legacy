f8ea96a57730448f88f09f52b00021621bda36b7
darkfire006 <blazer257@live.com>
2019-02-18 23:40:44 -0600

    alolan prep stuff

---
+++ P3D/Pokemon/Items/Standard/CellBattery.vb
@@ -0,0 +1,19 @@
+Namespace Items.Standard
+
+    <Item(595, "CellBattery")>
+    Public Class CellBattery
+
+        Inherits Item
+
+        Public Overrides ReadOnly Property Description As String = "An item to be held by a Pokémon. It boosts Attack if hit with an Electric-type attack. It can only be used once."
+        Public Overrides ReadOnly Property PokeDollarPrice As Integer = 200
+        Public Overrides ReadOnly Property CanBeUsedInBattle As Boolean = False
+        Public Overrides ReadOnly Property CanBeUsed As Boolean = False
+
+        Public Sub New()
+            _textureRectangle = New Rectangle(192, 312, 24, 24)
+        End Sub
+
+    End Class
+
+End Namespace
