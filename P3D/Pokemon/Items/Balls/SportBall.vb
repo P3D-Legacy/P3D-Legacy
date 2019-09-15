34122c1849107c45bafe3bb79584af73c5b14f27
Ruan Pablo <ruanpablo9k@gmail.com>
2019-09-15 05:39:34 -0300

    Preventing safari/sport ball smuggling

--- P3D/Pokemon/Items/Balls/SportBall.vb
+++ P3D/Pokemon/Items/Balls/SportBall.vb
@@ -5,7 +5,9 @@ Namespace Items.Balls
 
         Inherits BallItem
 
+        Public Overrides ReadOnly Property CanBeHold As Boolean = False
         Public Overrides ReadOnly Property Description As String = "A special Pokéball for the Bug-Catching Contest."
+        Public Overrides ReadOnly Property PokeDollarPrice As Integer = 200
         Public Overrides ReadOnly Property CatchMultiplier As Single = 1.5F
 
         Public Sub New()
