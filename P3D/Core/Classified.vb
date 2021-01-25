9147437d7b9f404f2537aa4ba2773db0c11c227b
darkfire006 <blazer257@live.com>
2021-01-25 11:04:16 -0600

    Merge Classified
    Merge Classified Class into GameController.vb

--- P3D/Core/Classified.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class Classified
-    Public Shared Remote_Texture_URL As String = "" ' CLASSIFIED
-    Public Const GameJolt_Game_ID As String = "" ' CLASSIFIED
-    Public Const GameJolt_Game_Key As String = "" ' CLASSIFIED
-    Public Shared Encryption_Password As String = "" ' CLASSIFIED
-End Class
--- P3D/Core/GameController.vb
+++ P3D/Core/GameController.vb
@@ -1,6 +1,13 @@
 ﻿Imports System.Windows.Forms
 Imports GameDevCommon
 
+Public Class Classified
+    Public Shared Remote_Texture_URL As String = "" ' CLASSIFIED
+    Public Const GameJolt_Game_ID As String = "" ' CLASSIFIED
+    Public Const GameJolt_Game_Key As String = "" ' CLASSIFIED
+    Public Shared Encryption_Password As String = "" ' CLASSIFIED
+End Class
+
 ''' <summary>
 ''' Controls the game's main workflow.
 ''' </summary>
