a368099b07623c841e81d73a4d72d452a23fdf4f
darkfire006 <blazer257@live.com>
2021-10-21 01:07:15 -0500

    insert start script into Kolben GM and create the script files for the reward system

--- P3D/Resources/GameModeManager.vb
+++ P3D/Resources/GameModeManager.vb
@@ -571,6 +571,8 @@ Public Class GameMode
         Dim gameMode As New GameMode("Pokemon 3D", "The normal game mode.", GameController.GAMEVERSION, "Kolben Games", "\Content\Data\maps\", "\Content\Data\Scripts\", "\Content\Data\maps\poke\", "\Content\Pokemon\Data\", "\Content\", "\Content\Localization\", New List(Of GameRule),
                                      "newgame\intro0.dat", New Vector3(1.0F, 0.1F, 3.0F), MathHelper.PiOver2, "Your Room", "", New Color(59, 123, 165), "0", "welcome", "1", SkinColors, SkinFiles, SkinNames, SkinGenders)
 
+        gameMode.StartScript = "startscript\main"
+
         Dim gameRules As New List(Of GameRule)
         gameRules.Add(New GameRule("MaxLevel", "100"))
         gameRules.Add(New GameRule("OnlyCaptureFirst", "0"))
