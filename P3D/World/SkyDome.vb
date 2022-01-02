38f2f0c0f4ea708cb0d35c6aac87ff378ca13aa7
JappaWakka <jjlspeelman@outlook.com>
2022-01-02 22:25:26 +0100

    Made the light color more neutral

--- P3D/World/SkyDome.vb
+++ P3D/World/SkyDome.vb
@@ -148,13 +148,13 @@
             End If
             Select Case Screen.Level.DayTime
                 Case 1
-                    Return New Color(48, 200, 248)
+                    Return New Color(128, 128, 128)
                 Case 2
-                    Return New Color(40, 88, 136)
+                    Return New Color(128, 128, 128)
                 Case 3
-                    Return New Color(168, 224, 248)
+                    Return New Color(128, 128, 128)
                 Case 4
-                    Return New Color(192, 152, 184)
+                    Return New Color(128, 128, 128)
             End Select
         End If
     End Function
