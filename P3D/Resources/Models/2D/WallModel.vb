13c838c9aaa3dcb7e2b0516b013e6637b276ecbb
JappaWakka <jjlspeelman@outlook.com>
2022-02-03 22:31:11 +0100

    Actually saved the WallModel file

--- P3D/Resources/Models/2D/WallModel.vb
+++ P3D/Resources/Models/2D/WallModel.vb
@@ -1,17 +1,17 @@
-﻿Public Class BillModel
+﻿Public Class WallModel
 
     Inherits BaseModel
 
     Public Sub New()
-        Me.ID = 3
+        Me.ID = 20
 
         Dim vertexData = New VertexPositionNormalTexture() {
-            New VertexPositionNormalTexture(New Vector3(-0.5, -0.5, 0), Vector3.Backward, New Vector2(0, 1)),
-            New VertexPositionNormalTexture(New Vector3(-0.5, 0.5, 0), Vector3.Backward, New Vector2(0, 0)),
-            New VertexPositionNormalTexture(New Vector3(0.5, 0.5, 0), Vector3.Backward, New Vector2(1, 0)),
-            New VertexPositionNormalTexture(New Vector3(0.5, 0.5, 0), Vector3.Backward, New Vector2(1, 0)),
-            New VertexPositionNormalTexture(New Vector3(0.5, -0.5, 0), Vector3.Backward, New Vector2(1, 1)),
-            New VertexPositionNormalTexture(New Vector3(-0.5, -0.5, 0), Vector3.Backward, New Vector2(0, 1))
+            New VertexPositionNormalTexture(New Vector3(-0.5, -0.5, 0.5), Vector3.Backward, New Vector2(0, 1)),
+            New VertexPositionNormalTexture(New Vector3(-0.5, 0.5, 0.5), Vector3.Backward, New Vector2(0, 0)),
+            New VertexPositionNormalTexture(New Vector3(0.5, 0.5, 0.5), Vector3.Backward, New Vector2(1, 0)),
+            New VertexPositionNormalTexture(New Vector3(0.5, 0.5, 0.5), Vector3.Backward, New Vector2(1, 0)),
+            New VertexPositionNormalTexture(New Vector3(0.5, -0.5, 0.5), Vector3.Backward, New Vector2(1, 1)),
+            New VertexPositionNormalTexture(New Vector3(-0.5, -0.5, 0.5), Vector3.Backward, New Vector2(0, 1))
         }
 
         Setup(vertexData)
