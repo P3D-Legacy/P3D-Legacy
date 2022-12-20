7a0b4a6a2192c4de0173aa6e2998c42b012aec6b
darkfire006 <blazer257@live.com>
2022-12-20 10:30:50 -0600

    mark explosive moves

--- P3D/Pokemon/Attacks/Normal/Explosion.vb
+++ P3D/Pokemon/Attacks/Normal/Explosion.vb
@@ -45,6 +45,7 @@
             Me.IsDamagingMove = True
             Me.IsProtectMove = False
             Me.IsSoundMove = False
+            Me.IsExplosiveMove = True
 
             Me.IsAffectedBySubstitute = True
             Me.IsOneHitKOMove = False
