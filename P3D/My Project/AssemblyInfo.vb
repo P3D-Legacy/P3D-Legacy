cec5c0c172b8ff03b69b62d5bf0068c598a505a2
darkfire006 <blazer257@live.com>
2023-04-26 12:26:16 -0500

    0.59.2 wrap up commit

--- P3D/My Project/AssemblyInfo.vb
+++ P3D/My Project/AssemblyInfo.vb
@@ -31,4 +31,4 @@ Imports System.Runtime.InteropServices
 '      Build Number
 '      Revision
 '
-<Assembly: AssemblyVersion("0.59.1")>
+<Assembly: AssemblyVersion("0.59.2")>
--- P3D/Security/FileValidation.vb
+++ P3D/Security/FileValidation.vb
@@ -6,8 +6,8 @@
         Shared _valid As Boolean = False
 
         Const RUNVALIDATION As Boolean = False
-        Const EXPECTEDSIZE As Integer = 51268031
-        Const METAHASH As String = "QzQxOTkzQzQ1Nzg4QUE4NEIzN0EzMDRBOEVDOEJBOTY="
+        Const EXPECTEDSIZE As Integer = 51403932
+        Const METAHASH As String = "N0U0MUM4NEFEOTY5MTkyRThFM0YyRTk5OTJGQTExNDE="
 
         Public Shared ReadOnly Property IsValid(ByVal ForceResult As Boolean) As Boolean
             Get
