a0e04eda442f6f3ed9af1156beed40d0ede7541d
darkfire006 <blazer257@live.com>
2023-05-28 23:18:08 -0500

    add sea plane item

---
+++ P3D/Pokemon/Items/KeyItems/SeaPlaneTicket.vb
@@ -0,0 +1,16 @@
+Namespace Items.KeyItems
+
+    <Item(655, "Sea Plane Ticket")>
+    Public Class SeaPlaneTicket
+
+        Inherits KeyItem
+
+        Public Overrides ReadOnly Property Description As String = "The ticket required for flying on the sea plane. It has a drawing of xxxx on it. "
+
+        Public Sub New()
+            _textureRectangle = New Rectangle(432, 408, 24, 24)
+        End Sub
+
+    End Class
+
+End Namespace
