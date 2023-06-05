ee2a304be2535f4bc49b393a243b775f68790562
JappaWakka <jjlspeelman@outlook.com>
2023-06-05 17:30:19 +0200

    Forgot a thing

--- P3D/Entites/Enviroment/Waterfall.vb
+++ P3D/Entites/Enviroment/Waterfall.vb
@@ -133,12 +133,8 @@
     Private Sub ChangeTexture()
         If Core.GameOptions.GraphicStyle = 1 Then
             If WaterfallTexturesTemp.Count = 0 Or WaterTexturesTemp.Count = 0 OrElse Waterfall.WaterTexturesTemp.ContainsKey("_0") = False OrElse Waterfall.WaterTexturesTemp.ContainsKey(AdditionalValue & "_0") = False Then
-
                 ClearAnimationResources()
                 AddDefaultWaterAnimationResources()
-                CreateWaterTextureTemp()
-            End If
-            ClearAnimationResources()
                 CreateWaterTextureTemp()
             End If
 
