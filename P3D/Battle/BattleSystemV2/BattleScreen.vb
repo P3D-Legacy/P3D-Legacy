62fea61236ab410f39646cdec7d4ef76e8286e45
JappaWakka <jjlspeelman@outlook.com>
2023-06-21 19:22:24 +0200

    Fix battlescreen having wrong daytime sky

--- P3D/Battle/BattleSystemV2/BattleScreen.vb
+++ P3D/Battle/BattleSystemV2/BattleScreen.vb
@@ -872,6 +872,7 @@
 
             Level.Load("battle\" & levelfile)
             Level.MapName = SavedOverworld.Level.MapName
+            Level.DayTime = World.GetTime + 1
         End Sub
 
 #End Region
