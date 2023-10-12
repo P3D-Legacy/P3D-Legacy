8a39146e3e87524da9c8d1db976c8f11b391e3ca
JappaWakka <jjlspeelman@outlook.com>
2023-10-12 19:33:23 +0200

    Fix Peat Block

--- P3D/Pokemon/Items/Standard/PeatBlock.vb
+++ P3D/Pokemon/Items/Standard/PeatBlock.vb
@@ -3,13 +3,11 @@ Namespace Items.Standard
     <Item(654, "Peat Block")>
     Public Class PeatBlock
 
-        Inherits Item
+        Inherits StoneItem
 
         Public Overrides ReadOnly Property Description As String = "A block of muddy material that can be used as fuel for burning when it is dried. It’s loved by a certain Pokémon."
         Public Overrides ReadOnly Property PokeDollarPrice As Integer = 1000
         Public Overrides ReadOnly Property FlingDamage As Integer = 80
-        Public Overrides ReadOnly Property CanBeUsedInBattle As Boolean = False
-        Public Overrides ReadOnly Property CanBeUsed As Boolean = False
 
         Public Sub New()
             _textureRectangle = New Rectangle(408, 408, 24, 24)
