becb55949857a5798bf9d1ab4e6497e8b112764a
JappaWakka <jjlspeelman@outlook.com>
2025-05-24 15:17:08 +0200

    Running shoes dutch correction + remove space

--- P3D/Pokemon/Items/KeyItems/SSTicket.vb
+++ P3D/Pokemon/Items/KeyItems/SSTicket.vb
@@ -5,7 +5,7 @@ Namespace Items.KeyItems
 
         Inherits KeyItem
 
-        Public Overrides ReadOnly Property Description As String = "The ticket required for sailing on the ferry S.S. Aqua. It has a drawing of a ship on it. "
+        Public Overrides ReadOnly Property Description As String = "The ticket required for sailing on the ferry S.S. Aqua. It has a drawing of a ship on it."
 
         Public Sub New()
             _textureRectangle = New Rectangle(240, 216, 24, 24)
