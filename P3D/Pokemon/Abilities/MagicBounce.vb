c5d40238fb4e789f1ca25a9baa4d54e3a482a0a5
JappaWakka <jjlspeelman@outlook.com>
2026-06-26 19:36:03 +0200

    Fixed error in Magic Bounce description

--- P3D/Pokemon/Abilities/MagicBounce.vb
+++ P3D/Pokemon/Abilities/MagicBounce.vb
@@ -5,7 +5,7 @@ Namespace Abilities
         Inherits Ability
 
         Public Sub New()
-            MyBase.New(156, "Magic Bounce", "Reflects status- changing moves.")
+            MyBase.New(156, "Magic Bounce", "Reflects status-changing moves.")
         End Sub
 
     End Class
