37650b1579c9a731e7a32d631cd6e7c87cec7f29
JappaWakka <jjlspeelman@outlook.com>
2026-07-10 17:58:18 +0200

    Fix Jump Ledges being jumpable when an entity is in the way or colliding

--- P3D/Entities/Environment/StepBlock.vb
+++ P3D/Entities/Environment/StepBlock.vb
@@ -9,28 +9,51 @@
             facing += 4
         End If
 
-        If Screen.Camera.GetPlayerFacingDirection() = facing Then 
-            'Dim newPos As New Vector3(Screen.Camera.Position.X + Screen.Camera.moveDirectionX * 2, Screen.Camera.Position.Y, Screen.Camera.Position.Z + Screen.Camera.moveDirectionZ * 2)
-            'If CType(Screen.Camera, OverworldCamera).CheckCollision(newPos) = True Then
-
-            'End If
-            Screen.Camera.AddToPlannedMovement(New Vector3(0, 0.15F, 0))
-            Screen.Camera.Move(1.0F)
-            Screen.Level.OverworldPokemon.Visible = False
-            Screen.Level.OverworldPokemon.warped = True
-
-            Dim delay As Date = Date.Now
-            If Screen.Level.Riding = True Then
-                delay = Date.Now.AddMilliseconds(401)
-            ElseIf Core.Player.IsRunning() = True Then
-                delay = Date.Now.AddMilliseconds(540)
-            Else
-                delay = Date.Now.AddMilliseconds(867)
+        Dim CanJumpLedge As Boolean = True
+        For Each Entity As Entity In Screen.Level.Entities
+            If Entity.Collision = True And Entity.GetType <> GetType(StepBlock) Then
+                If Entity.boundingBox.Contains(Me.Position) = ContainmentType.Contains Then
+                    CanJumpLedge = False
+                End If
             End If
+        Next
 
-            SoundManager.PlaySound("jump_ledge", False, delay)
+        Dim checkBlockedPosition As Vector3 = Screen.Camera.GetForwardMovedPosition()
+        checkBlockedPosition.X += Screen.Camera.GetMoveDirection().X
+        checkBlockedPosition.Z += Screen.Camera.GetMoveDirection().Z
+        Dim blockEntity As Entity = GetEntity(Screen.Level.Entities, checkBlockedPosition, True, {GetType(NPC), GetType(StrengthRock)})
 
-            Return False
+        If blockEntity IsNot Nothing Then
+            If blockEntity.Collision = True Then
+                CanJumpLedge = False
+            End If
+        End If
+
+        If CanJumpLedge = True Then
+
+            If Screen.Camera.GetPlayerFacingDirection() = facing Then
+                'Dim newPos As New Vector3(Screen.Camera.Position.X + Screen.Camera.moveDirectionX * 2, Screen.Camera.Position.Y, Screen.Camera.Position.Z + Screen.Camera.moveDirectionZ * 2)
+                'If CType(Screen.Camera, OverworldCamera).CheckCollision(newPos) = True Then
+
+                'End If
+                Screen.Camera.AddToPlannedMovement(New Vector3(0, 0.15F, 0))
+                Screen.Camera.Move(1.0F)
+                Screen.Level.OverworldPokemon.Visible = False
+                Screen.Level.OverworldPokemon.warped = True
+
+                Dim delay As Date = Date.Now
+                If Screen.Level.Riding = True Then
+                    delay = Date.Now.AddMilliseconds(401)
+                ElseIf Core.Player.IsRunning() = True Then
+                    delay = Date.Now.AddMilliseconds(540)
+                Else
+                    delay = Date.Now.AddMilliseconds(867)
+                End If
+
+                SoundManager.PlaySound("jump_ledge", False, delay)
+
+                Return False
+            End If
         End If
         Return True
     End Function
