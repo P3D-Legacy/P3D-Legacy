1b63a0e72c67af9fc5411d7cda3015fb7a5625d5
darkfire <blazer257@live.com>
2026-08-15 19:01:45 -0500

    update inner focus text

--- P3D/Pokemon/Abilities/InnerFocus.vb
+++ P3D/Pokemon/Abilities/InnerFocus.vb
@@ -5,7 +5,7 @@ Namespace Abilities
         Inherits Ability
 
         Public Sub New()
-            MyBase.New(39, "Inner Focus", "The Pokémon is protected from flinching.")
+            MyBase.New(39, "Inner Focus", "The Pokémon's intense focus prevents it from flinching or being affected by Intimidate.")
         End Sub
 
     End Class
